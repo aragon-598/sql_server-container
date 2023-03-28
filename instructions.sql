e67cca2c312d0d7bca045cecc3190599227d6086
Alejandro Aragon <aragondru155@gmail.com>
2023-03-27 23:26:54 -0600

    sql server container and instructions to restore .bak

---
+++ instructions.sql
@@ -0,0 +1,11 @@
+--PASO 1
+use master;
+
+--PASO 2
+RESTORE FILELISTONLY FROM DISK = '/var/opt/mssql/backup/AdventureWorks2019.bak';
+
+--PASO 3
+RESTORE DATABASE new_db
+FROM DISK = '/var/opt/mssql/backup/AdventureWorks2019.bak' WITH FILE = 1,
+MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2017.mdf',
+MOVE 'AdventureWorks2017_log' TO '/var/opt/mssql/data/AdventureWorks2017_log.ldf';
